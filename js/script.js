9276297fa07c3a59019463543a526e63ca4f75dd
justinhessdev <justinhessdev@gmail.com>
2016-12-02 11:32:58 -0800

    added score logic and game over logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// array that contains each player's board
-var playerBoardArray=[$('#playerOneBoard'), $('#playerTwoBoard')];
+// array that contains references to each player's board
+var $playerBoardArray=[$('#playerOneBoard'), $('#playerTwoBoard')];
+
+// array that contains references to each player's score 
+var $playerScoreArray=[$('#playerOneScoreBoard'), $('#playerTwoScoreBoard')];
+
+var playerScoreTracker = [{player: "Player 1", score: "0"}, {player: "Player 2", score: "0"}];
 
 // represents which player has turn
 var currentPlayerIndex = 0;
@@ -60,8 +65,10 @@ function tileLogic() {
 function wordLogic() {
   var $selectedTiles = $('.selectedTile');
   if (words.indexOf($clickedWord.text()) >= 0){
+    playerScoreTracker[currentPlayerIndex].score = parseInt(playerScoreTracker[currentPlayerIndex].score) + $clickedWord.text().length;
+    $playerScoreArray[currentPlayerIndex].text("Score: " + parseInt(playerScoreTracker[currentPlayerIndex].score));
     // represents clicked tiles
-    playerBoardArray[currentPlayerIndex].append('<div class="addedWordClass"><p class="addedWordP">' + $clickedWord.text() + '</p></div>');
+    $playerBoardArray[currentPlayerIndex].append('<div class="addedWordClass"><p class="addedWordP">' + $clickedWord.text() + '</p></div>');
     $clickedWord.text("");
     $selectedTiles.addClass("greenTile");
     $greenInterval = setInterval(removeGreen, 500);
@@ -127,14 +134,16 @@ function reset() {
   $clickedWord.text("");
 }
 
-  /*
-    NEEDS TO BE IMPLEMENTED
-  */
+/*
+  NEEDS TO BE IMPLEMENTED
+*/
 function gameOverLogic() {
   var currentPlayer = $('#currentPlayer');
   currentPlayer.text("Game Over");
+  var gameIndex = indexOfMaxTwoPlayers(playerScoreTracker);
+  $('#gameRules').text(playerScoreTracker[gameIndex[0]].player + " wins the game");
+  $('#funnyMessage').text(playerScoreTracker[gameIndex[1]].player + " was stupid");
   $countdownTimer.hide();
-  $countdownTimer.text("5");
   $('.addedWordClass').show();
   $hiddenImageArr[0].hide();
   $tiles.off();
@@ -144,6 +153,42 @@ function gameOverLogic() {
   $clickedWord.text("");
   $startGame.hide();
   $newGame.show();
+}
+
+// returns index of largest number -- in our case the index of the player with the highest score
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0].score;
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i].score > max) {
+            maxIndex = i;
+            max = arr[i].score;
+        }
+    }
+
+    return maxIndex;
+}
+
+function indexOfMaxTwoPlayers(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0].score;
+    var maxIndex = 0;
+    var lowIndex = 1;
+
+    if (arr[1].score > max) {
+        maxIndex = 1;
+        lowIndex = 0;
+    }
+
+    return [maxIndex, lowIndex];
 }
 
 /*
