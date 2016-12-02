d7594bb6b760b2a4e775263cb0244f004e212330
justinhessdev <justinhessdev@gmail.com>
2016-12-01 23:15:39 -0800

    adding game ending logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// represent a player
+// array that contains each player's board
 var playerBoardArray=[$('#playerOneBoard'), $('#playerTwoBoard')];
 
 // represents which player has turn
@@ -14,16 +14,32 @@ var letterArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 // represents the area where clicked letters appear on board
 var $clickedWord = $('#clickedWord');
 
+// represents the 9 tiles on the board
+var $tiles = $('.tile');
+
 // global variable to keep track of interval and stop timer at 0
 var $myInterval;
 
-// keeps track of red tiles and stops interval when completeß
+// keeps track of red tiles and stops interval when complete
 var $redInterval;
+
+// keeps track of green tiles and stops interval when complete
+var $greenInterval;
+
+// represents the countdown timer
+var $countdownTimer = $('#countdownTimer');
+
+// represents the lock image for hiding the word list
+var $hiddenImageArr = [$('#hiddenImageOne'), $('#hiddenImageTwo')];
+
+var $newGame = $('#newGameButton');
+
+var $startGame = $('#startButton');
 
 // fill each tile a random letter from the alphabet
 function fillTileWithRandomLetter() {
-  for(var i=0; i<$('.tile').length; i++){
-      $('.tile')[i].innerHTML = letterArr[Math.floor(Math.random()*letterArr.length)];
+  for(var i=0; i<$tiles.length; i++){
+      $tiles[i].innerHTML = letterArr[Math.floor(Math.random()*letterArr.length)];
   }
 }
 
@@ -38,19 +54,21 @@ function tileLogic() {
     $clickedWord.text($clickedWord.text() + $(this).text());
   }
 
-    $(this).toggleClass('selectedTile');
-
+  $(this).toggleClass('selectedTile');
 }
 
 function wordLogic() {
+  var $selectedTiles = $('.selectedTile');
   if (words.indexOf($clickedWord.text()) >= 0){
+    // represents clicked tiles
     playerBoardArray[currentPlayerIndex].append('<div class="addedWordClass"><p class="addedWordP">' + $clickedWord.text() + '</p></div>');
     $clickedWord.text("");
-    $('.selectedTile').slideUp("fast", repopulateLogic);
+    $selectedTiles.addClass("greenTile");
+    $greenInterval = setInterval(removeGreen, 500);
   } else {
     $clickedWord.text("");
-    $('.selectedTile').addClass("redTile");
-    $redInterval = setInterval(removeRed, 1000);
+    $selectedTiles.addClass("redTile");
+    $redInterval = setInterval(removeRed, 500);
   }
 }
 
@@ -61,18 +79,21 @@ function removeRed() {
   clearInterval($redInterval);
 }
 
+function removeGreen() {
+  var $selectedTiles = $('.selectedTile');
+  $selectedTiles.removeClass("greenTile");
+  $selectedTiles.removeClass("selectedTile");
+  clearInterval($greenInterval);
+}
+
 function startLogic() {
+  $tiles.on('click', tileLogic);
   $('#navBar').slideUp("fast", showTimer);
+  $hiddenImageArr[currentPlayerIndex].hide();
 }
 
 function showTimer() {
-  $('#countdownTimer').slideDown("fast", startInterval);
-}
-
-function repopulateLogic() {
-  var $selectedTiles = $('.selectedTile');
-  $selectedTiles.slideDown("fast");
-  $selectedTiles.removeClass("selectedTile");
+  $countdownTimer.slideDown("fast", startInterval);
 }
 
 function startInterval() {
@@ -80,32 +101,57 @@ function startInterval() {
 }
 
 function startTimer() {
-  var $countdownTimer = $('#countdownTimer');
   $countdownTimer.text($countdownTimer.text()-1);
-
   if($countdownTimer.text() === "0"){
     clearInterval($myInterval);
-    currentPlayerIndex = 1;
-    $('#navBar').slideDown("fast", reset);
+    currentPlayerIndex++;
+    if(currentPlayerIndex>1){
+      $('#navBar').slideDown("fast", gameOverLogic);
+    } else {
+      $('#navBar').slideDown("fast", reset);
+    }
   }
-
 }
 
 function reset() {
   var currentPlayer = $('#currentPlayer');
   currentPlayer.text("Player " + (currentPlayerIndex+1));
-  var $countdownTimer = $('#countdownTimer');
   $countdownTimer.hide();
   $countdownTimer.text("5");
   $('.addedWordClass').hide();
+  $hiddenImageArr[0].show();
+  $tiles.off();
+  $tiles.removeClass("selectedTile");
+  $tiles.removeClass("redTile");
+  $tiles.removeClass("greenTile");
+  $clickedWord.text("");
+}
+
+  /*
+    NEEDS TO BE IMPLEMENTED
+  */
+function gameOverLogic() {
+  var currentPlayer = $('#currentPlayer');
+  currentPlayer.text("Game Over");
+  $countdownTimer.hide();
+  $countdownTimer.text("5");
+  $('.addedWordClass').show();
+  $hiddenImageArr[0].hide();
+  $tiles.off();
+  $tiles.removeClass("selectedTile");
+  $tiles.removeClass("redTile");
+  $tiles.removeClass("greenTile");
+  $clickedWord.text("");
+  $startGame.hide();
+  $newGame.show();
 }
 
 /*
  * IMPLEMENTING FUNCTIONS
  */
 
-$('#countdownTimer').hide();
-$('.tile').on('click', tileLogic);
+$newGame.hide();
+$countdownTimer.hide();
 $('#enterButton').on('click', wordLogic);
-$('#startButton').on('click', startLogic);
-fillTileWithRandomLetter();
+$startGame.on('click', startLogic);
+fillTileWithRandomLetter()
